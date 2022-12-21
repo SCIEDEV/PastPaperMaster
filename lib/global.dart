2b1a3aba4da4a6c2c5d07e74a8eaae376732a493
Micfong <micfong2@outlook.com>
2022-12-21 17:24:22 +0800

    style: clean unnecessary comment

---
+++ lib/global.dart
@@ -0,0 +1,8 @@
+library ppm.globals;
+
+import 'package:flutter/material.dart';
+
+/// Global `BuildContext` used for retriving providers.
+///
+/// This should always be set to the `context` which `MaterialApp` lies in.
+late BuildContext globalContext;
